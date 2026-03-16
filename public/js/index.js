a0b4f4abb1f4c341bf4f20e4a28562e194f43473
SAOT31 <saot-31@hotmail.com>
2026-03-16 13:28:17 -0500

    validating emails on frontend

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,8 @@
 // This runs on the landing page (index.html).
 // If the user is already logged in, I send them straight to their dashboard
+// so they never see the landing page again unnecessarily.
+const parkly_session = JSON.parse(localStorage.getItem('parkly_session'));
+if (!parkly_session) {
+    console.log("Landing Page");
+} else if (parkly_session.role == 'client') {
+    window.location.href = './search.html';
