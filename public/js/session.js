08558c979620a773a2d425616e41711e857c5fb9
SAOT31 <saot-31@hotmail.com>
2026-03-16 13:28:19 -0500

    adding click event listeners

--- public/js/session.js
+++ public/js/session.js
@@ -8,3 +8,5 @@ if (!parkly_session) {
     window.location.href = './search.html';
 } else if (parkly_session.role == 'owner') {
     window.location.href = './owner-dash.html';
+} else if (parkly_session.role == 'admin') {
+    window.location.href = './admin-dash.html';
